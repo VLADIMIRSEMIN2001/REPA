4553de9da990050728fc0c8df85c81ab4e8daaae
VLADIMIRSEMIN2001 <Vova_Semin007@mail.ru>
2022-12-05 16:11:52 +0500

    444444

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+
+{
+    int a;
+    int b;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    return 0;
+ }
